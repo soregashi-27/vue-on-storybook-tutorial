715c4125718d6106cff475b50910cb0473f0ca25
soregashi-27 <tomo.soul.15@gmail.com>
2021-06-27 17:59:56 +0900

    Storyshotsを追加

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,11 +1,14 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
+import initStoryshots from '@storybook/addon-storyshots';
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
+initStoryshots();
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
   });
